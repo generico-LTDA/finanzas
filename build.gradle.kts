8d075324930fa673312708aaf493a01b9fd493d9
Carlos Núñez <carlosnr3005@gmail.com>
2023-12-24 10:54:56 -0300

    feat: add build-logic with settings and build gradle files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.kotlin) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.com.android.library) apply false
 }
